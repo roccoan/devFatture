e3e8f5dad6ec3126dbfe9d85e42fbc9c51f0b189
Rocco Annicchiarico <80759796+roccoan@users.noreply.github.com>
2023-02-03 13:50:39 +0100

    Update generapdf.js

--- modal/generapdf.js
+++ modal/generapdf.js
@@ -1,5 +1,5 @@
 window.jsPDF = window.jspdf.jsPDF;
-var submit = document.getElementById('submit')
+var submit = document.getElementById('genera')
 submit.addEventListener("click", createPDF)
 
 //calcoli & layout fattura & salvaPDF
@@ -44,8 +44,8 @@ function createPDF() {
     var Descrizione3 = document.querySelector('input[name="Descrizione3"]').value
     var Unitario3 = document.querySelector('input[name="Unitario3"]').value
 
-    var INPS = document.querySelector('input[id="INPS"]').value
-    var MarcaDaBollo = document.querySelector('input[id="MarcaDaBollo"]').value
+    var INPS = document.getElementById("INPS").checked
+    var MarcaDaBollo = document.getElementById("MarcaDaBollo").checked
 
     //campi pag.4
     var data = document.querySelector('input[name="Emissione"]').value 
@@ -59,96 +59,86 @@ function createPDF() {
     var INPSval
     var MarcaDaBolloVal
 
-    if (INPS.value == true && MarcaDaBollo == true) {
+    if (INPS == true && MarcaDaBollo == true) {
         INPSper = 0.04
         MarcaDaBolloVal = 2
+        subtotal = (Ore*Unitario)+(Ore2*Unitario2)+(Ore3*Unitario3)
+        INPSval = subtotal*INPSper
+        totale = subtotal+INPSval+MarcaDaBolloVal
        } else if (INPS == true && MarcaDaBollo == false) {
         INPSper = 0.04
         MarcaDaBolloVal = 0
+        subtotal = (Ore*Unitario)+(Ore2*Unitario2)+(Ore3*Unitario3)
+        INPSval = subtotal*INPSper
+        totale = subtotal+INPSval+MarcaDaBolloVal
        } else if (INPS == false && MarcaDaBollo == true) {
         INPSper = 0
         MarcaDaBolloVal = 2
+        subtotal = (Ore*Unitario)+(Ore2*Unitario2)+(Ore3*Unitario3)
+        INPSval = subtotal*INPSper
+        totale = subtotal+INPSval+MarcaDaBolloVal
        } else {
         INPSper = 0
-        MarcaDaBolloVal = 0 
+        MarcaDaBolloVal = 0
+        subtotal = (Ore*Unitario)+(Ore2*Unitario2)+(Ore3*Unitario3)
+        INPSval = subtotal*INPSper
+        totale = subtotal+INPSval+MarcaDaBolloVal 
        }
 
     //Calcolo totale
-    subtotal = (Ore*Unitario)+(Ore2*Unitario2)+(Ore3*Unitario3)
-    INPSval = subtotal*INPSper
-    totale = subtotal+INPSval+MarcaDaBolloVal
+    //subtotal = (Ore*Unitario)+(Ore2*Unitario2)+(Ore3*Unitario3)
+    //INPSval = subtotal*INPSper
+    //totale = subtotal+INPSval+MarcaDaBolloVal
 
     //layout fattura
     // Aggiunge il numero e la data della fattura
     doc.setFont('helvetica');
-    doc.setFontSize(30);
-    doc.text(30, 30, "Fattura n." + numFattura + " del " + dataFattura);
+    doc.setFontSize(35);
+    doc.text(28, 30, "Fattura n." + numFattura + " del " + dataFattura);
 
     // Aggiunge i dati del professionista e del paziente alla fattura
     doc.setFontSize(14);
     doc.text(20, 50, "Professionista:");
 
-    doc.setFontSize(11);
-    doc.text(20, 60, fullname);
-    doc.text(20, 70, tuoIndirizzo);
-    doc.text(20, 80, "P. IVA:" + tuaPiva);
+    doc.setFontSize(12);
+    doc.text(20, 58, fullname);
+    doc.text(20, 63, tuoIndirizzo);
+    doc.text(20, 68, "P. IVA:" + tuaPiva);
 
     doc.setFontSize(14);
-    doc.text(120, 50, "Ricevente:");
+    doc.text(20, 80, "Destinatario:");
 
-    doc.setFontSize(11);
-    doc.text(120, 60, nomePaziente);
-    doc.text(120, 70, indirizzoPaziente);
-    doc.text(120, 80, taxCode);
+    doc.setFontSize(12);
+    doc.text(20, 88, nomePaziente);
+    doc.text(20, 93, indirizzoPaziente);
+    doc.text(20, 98, taxCode);
 
     // Disegna il riquadro per la marca da bollo
-    doc.rect(100, 90, 60, 45);
-
+    doc.rect(120, 108, 60, 45);
+    doc.text(125, 132, "Marca da Bollo ove prevista")
+    
+    doc.setFontSize(14);
     doc.autoTable({
+        headStyles: { halign: 'left' },
+        bodyStyles: { halign: 'left' },
+        head: [['Quantità/Ore', 'Descrizione', 'Prezzo']],
         body: [
-            ['Quantità', 'Descrizione', 'Prezzo'],
-            [Ore, Descrizione, Unitario],
-            [Ore2, Descrizione2, Unitario2],
-            [Ore3, Descrizione3, Unitario3],
-            [],
+            [Ore, Descrizione, "€ " + Unitario],
+            [Ore2, Descrizione2, "€ " + Unitario2],
+            [Ore3, Descrizione3, "€ " + Unitario3],
+            ["","",""],
             ["","Subtotale:", "€ " + subtotal],
             ["","4% INPS:", "€ " + INPSval],
             ["", "Marca Da Bollo:", "€ " + MarcaDaBolloVal],
             ["", "Totale:", "€ " + totale]
             ],
-        startY: 140,
-        theme: 'grid',
+        startY: 170,
                  })
 
-    /*doc.text(30, 90, "Quantità");
-    doc.text(65, 90, "Descrizione");
-    doc.text(150, 90, "Prezzo Unitario");
-
-    doc.text(30, 100, Ore);
-    doc.text(100, 100, Descrizione);
-    doc.text(150, 100, "€ " + Unitario);
-
-    if (Ore2 != 0) {
-        doc.text(30, 110, Ore2);
-        doc.text(100, 110, Descrizione2);
-        doc.text(160, 110, "€ " + Unitario2);
-    } else {} 
-    if (Ore3 != 0) {
-        doc.text(30, 120, Ore3);
-        doc.text(100, 120, Descrizione3);
-        doc.text(160, 120, "€ " + Unitario3);
-    } else {} 
-
-    //Aggiunge il subtotale e l'importo della marca da bollo
-    doc.text(100, 130, "Subtotale: " + subtotal + " €");
-    doc.text(100, 140, "Marca da Bollo: " + MarcaDaBolloVal + " €");
-    doc.text(100, 150, "Rivalsa INPS: " + INPSval + " €");
-    doc.text(100, 160, "Totale Fattura: " + totale + " €");*/
-
-    doc.setFontSize(8);
-    doc.text(20, 250, "Operazione effettuata senza applicazione dell’IVA ai sensi dell’art.1, commi da 54 a 89, Legge n.190/2014 - Regime Forfettario.");
-    doc.text(20, 260, "Si richiede la non applicazione della Ritenuta alla fonte a titolo di acconto come previsto dall’art. 1, comma 67, Legge n.190/2014.");
-    doc.text(20, 270, "Imposta di Bollo sull’originale (per importi superiori ad €77,47).")
+    doc.setFontSize(9);
+    doc.text(15, 270, "Operazione effettuata senza applicazione dell’IVA ai sensi dell’art.1, commi da 54 a 89, Legge n.190/2014 - Regime Forfettario.");
+    doc.text(15, 275, "Si richiede la non applicazione della Ritenuta alla fonte a titolo di acconto come previsto dall’art. 1, comma 67, Legge n.190/2014.");
+    doc.text(15, 280, "Imposta di Bollo sull’originale (per importi superiori ad €77,47).")
 
     //salva file con naming convention
     var filename = 'Fattura n.'+numFattura+' del '+dataFattura+', '+nomePaziente+'.pdf'
