77bce092c9ea056d89f576623f92b3b80f6bfe24
Rocco Annicchiarico <80759796+roccoan@users.noreply.github.com>
2023-02-07 23:16:43 +0100

    Update generapdf.js

--- modal/generapdf.js
+++ modal/generapdf.js
@@ -85,11 +85,6 @@ function createPDF() {
         totale = subtotal+INPSval+MarcaDaBolloVal 
        }
 
-    //Calcolo totale
-    //subtotal = (Ore*Unitario)+(Ore2*Unitario2)+(Ore3*Unitario3)
-    //INPSval = subtotal*INPSper
-    //totale = subtotal+INPSval+MarcaDaBolloVal
-
     //layout fattura
     // Aggiunge il numero e la data della fattura
     doc.setFont('helvetica');
@@ -97,31 +92,37 @@ function createPDF() {
     doc.text(28, 30, "Fattura n." + numFattura + " del " + dataFattura);
 
     // Aggiunge i dati del professionista e del paziente alla fattura
+    doc.setFont('helvetica');
     doc.setFontSize(14);
     doc.text(20, 50, "Professionista:");
 
+    doc.setFont('courier');
     doc.setFontSize(12);
     doc.text(20, 58, fullname);
     doc.text(20, 63, tuoIndirizzo);
     doc.text(20, 68, "P. IVA:" + tuaPiva);
-
+    
+    doc.setFont('helvetica');
     doc.setFontSize(14);
     doc.text(20, 80, "Destinatario:");
 
+    doc.setFont('courier');
     doc.setFontSize(12);
     doc.text(20, 88, nomePaziente);
     doc.text(20, 93, indirizzoPaziente);
     doc.text(20, 98, taxCode);
 
     // Disegna il riquadro per la marca da bollo
+    doc.setFont('helvetica');
     doc.rect(120, 108, 60, 45);
     doc.text(125, 132, "Marca da Bollo ove prevista")
     
     doc.setFontSize(14);
     doc.autoTable({
-        headStyles: { halign: 'left' },
+        headStyles: { halign: 'center' },
         bodyStyles: { halign: 'left' },
-        head: [['Quantità/Ore', 'Descrizione', 'Prezzo']],
+        theme: 'grid',
+        head: [['Quantità/Ore', 'Descrizione', 'Prezzo Unitario']],
         body: [
             [Ore, Descrizione, "€ " + Unitario],
             [Ore2, Descrizione2, "€ " + Unitario2],
@@ -133,14 +134,19 @@ function createPDF() {
             ["", "Totale:", "€ " + totale]
             ],
         startY: 170,
+        columnStyles: { 0: { halign: 'center' }, 1: { halign: 'center' } },
                  })
 
     doc.setFontSize(9);
     doc.text(15, 270, "Operazione effettuata senza applicazione dell’IVA ai sensi dell’art.1, commi da 54 a 89, Legge n.190/2014 - Regime Forfettario.");
-    doc.text(15, 275, "Si richiede la non applicazione della Ritenuta alla fonte a titolo di acconto come previsto dall’art. 1, comma 67, Legge n.190/2014.");
+    doc.text(15, 275, "Si richiede la non applicazione della Ritenuta alla fonte a titolo di acconto come previsto dall’art.1, comma 67, Legge n.190/2014.");
     doc.text(15, 280, "Imposta di Bollo sull’originale (per importi superiori ad €77,47).")
 
     //salva file con naming convention
     var filename = 'Fattura n.'+numFattura+' del '+dataFattura+', '+nomePaziente+'.pdf'
     doc.save(filename);
+
+    //torna in alto
+    var scroll = document.getElementById("navbar");
+    scroll.scrollIntoView();
 }
